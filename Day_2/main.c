13931677c5d26504125ffa30185039ae106b51b5
Radosław Głasek <01180779@pw.edu.pl>
2024-12-22 15:29:14 +0100

    added Day 2 part 2

--- Day_2/main.c
+++ Day_2/main.c
@@ -42,6 +42,63 @@ int part1(vec* v)
     return 1;
 }
 
+int part2_rec(vec* v, int skip) {
+    int type;
+    if (skip == 0 && v->size > 2) {
+        if (*vecAt(v, 2) - *vecAt(v, 1) > 0)
+            type = INCREASING;
+        else
+            type = DECREASING;
+    } else if (skip == 1 && v->size > 2) {
+        if (*vecAt(v, 2) - *vecAt(v, 0) > 0)
+            type = INCREASING;
+        else
+            type = DECREASING;
+    }
+    else {
+        if (*vecAt(v, 1) - *vecAt(v, 0) > 0)
+            type = INCREASING;
+        else
+            type = DECREASING;
+    }
+
+    long long int diff;
+    for (int i = 0; i < v->size - 1; ++i) {
+        if (i + 1 == skip || i == skip)
+            continue;
+        diff = *vecAt(v, i + 1) - *vecAt(v, i);
+        if (type == INCREASING && diff < 1) {
+            return 0;
+        }
+        else if (type == DECREASING && diff > -1) {
+            return 0;
+        }
+        else if (llabs(diff) > 3)
+            return 0;
+    }
+
+    if (skip - 1 >= 0 && skip + 1 < v->size) {
+        diff = *vecAt(v, skip + 1) - *vecAt(v, skip - 1);
+        if (type == INCREASING && diff < 1) {
+            return 0;
+        }
+        else if (type == DECREASING && diff > -1) {
+            return 0;
+        }
+        else if (llabs(diff) > 3)
+            return 0;
+    }
+    return 1;
+}
+
+int part2(vec* v) {
+    for (int i = 0; i < v->size; ++i) {
+        if (part2_rec(v, i))
+            return 1;
+    }
+    return 0;
+}
+
 int main() {
     vec v, temp;
     vecInit(&v);
@@ -55,6 +112,7 @@ int main() {
     for (int i = 0; i < n; ++i) {
         scanf_report(&v);
         res_part1 += part1(&v);
+        res_part2 += part2(&v);
     }
     printf("\n\nPart1: Result: %lld\nPart2: Result: %lld\n", res_part1, res_part2);
 
